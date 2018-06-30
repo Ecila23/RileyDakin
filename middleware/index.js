75e9077301e3b309c61cd05bef00662334ba6e4e
Alice-Claire Earle <aliceclaire@Alice-Claires-MacBook-Pro.local>
2018-06-30 14:46:21 +0100

    removed unused middleware

--- middleware/index.js
+++ middleware/index.js
@@ -9,24 +9,6 @@ middlewareObj.isLoggedIn = function(req, res, next){
     res.redirect("/register");
 };
 
-// middlewareObj.checkRsvpOwnership = function(req, res, next){
-//   if(req.isAuthenticated()) {
-//       Rsvp.findById(req.params.id, function(err, foundRsvp){
-//           if(err){
-//               res.redirect("back");
-//           } else {
-//               if(foundRsvp.user.id.equals(req.user._id)){
-//                   next();
-//               } else {
-//                   res.redirect("back");
-//               }
-//           }
-//       })
-//   } else {
-//       res.redirect("back");
-//   }  
-// };
-
 middlewareObj.checkRsvpOwnership = function(req, res, next){
   if(req.isAuthenticated()) {
       Rsvp.findById(req.params.id, function(err, foundRsvp){
